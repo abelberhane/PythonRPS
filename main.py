262cb37c587387fa3a28fd596bef8ac42373ca30
Abel <abelberhane@gmail.com>
2017-12-29 09:41:20 -0500

    Initial Push - Players added

---
+++ main.py
@@ -0,0 +1,21 @@
+'''
+
+    Rock, Paper, Scissors Game - Abel's Dojo
+
+'''
+
+import sys
+
+
+
+''' 
+    The Players
+'''
+user1 = input("What's your name?")
+user2 = input("And your name?")
+user1_answer = input("%s, do you want Rock, Paper or Scissors?" % user1)
+user2_answer = input("%s, do you want Rock, Paper or Scissors?" % user2)
+
+''' 
+    The Game
+'''
