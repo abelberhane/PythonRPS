20d82c76dd608676e2d366e1b78d6efab71be677
Abel <abelberhane@gmail.com>
2017-12-29 10:05:51 -0500

    Continued progress on Code

--- main.py
+++ main.py
@@ -19,3 +19,31 @@ user2_answer = input("%s, do you want Rock, Paper or Scissors?" % user2)
 ''' 
     The Game
 '''
+def compare(u1, u2):
+    if u1 == u2:
+        return("It's a tie!")
+    elif u1 =='rock':
+        if u2 == 'scissors':
+            return("Rock Wins!")
+        else:
+            return("Paper Wins!")
+    elif u1 == 'scissors':
+        if u2 == 'paper':
+            return ("Scissors Win!)")
+        else:
+            return("Rock Wins!")
+    elif u1 == 'paper':
+        if u2 == 'rock':
+            return("Paper Wins!")
+        else:
+            return("Scissors Wins!")
+    else:
+        return("Invalid Input! Try again.")
+    sys.exit()
+
+    ''' 
+        The Results
+    '''
+    print(compare(user1_answer, user2_answer))
+
+
